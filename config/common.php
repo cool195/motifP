92a17955bf12b637b9de878773f94bfa694b2088
zhangtao <250574087@qq.com>
2016-07-25 18:35:46 +0800

    商品详情页套页面

---
+++ config/common.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'APP_Api_Image' => "https://s3-us-west-1.amazonaws.com/emimagetest"
+];
--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -25,130 +25,67 @@
         <div class="row">
             <div class="col-lg-6 col-md-12">
                 <div class="p-a-20x box-shadow bg-white">
-                    <img class="img-fluid product-bigImg" src="/images/product/product.jpg" alt="">
+                    <img class="img-fluid product-bigImg" src="{{config('common.APP_Api_Image')}}/n1/{{$data['main_image_url']}}" alt="">
                     <div class="swiper-container">
                         <div class="productImg-list p-t-20x swiper-wrapper">
-                            <div class="productImg-item swiper-slide p-r-10x">
-                                <img class="img-thumbnail active" src="/images/product/product.jpg" width="110"
-                                     height="110" alt="商品图片">
-                            </div>
-                            <div class="productImg-item swiper-slide p-r-10x">
-                                <img class="img-thumbnail" src="/images/product/product1.jpg" width="110" height="110"
-                                     alt="商品图片">
-                            </div>
-                            <div class="productImg-item swiper-slide p-r-10x">
-                                <img class="img-thumbnail" src="/images/product/product.jpg" width="110" height="110"
-                                     alt="商品图片">
-                            </div>
-                            <div class="productImg-item swiper-slide p-r-10x">
-                                <img class="img-thumbnail" src="/images/product/product.jpg" width="110" height="110"
-                                     alt="商品图片">
-                            </div>
-                            <div class="productImg-item swiper-slide p-r-10x">
-                                <img class="img-thumbnail" src="/images/product/product.jpg" width="110" height="110"
-                                     alt="商品图片">
-                            </div>
+                            @if(isset($data['productImages']))
+                                @foreach($data['productImages'] as $image)
+                                    <div class="productImg-item swiper-slide p-r-10x">
+                                        <img class="img-thumbnail active" src="{{config('common.APP_Api_Image')}}/n1/{{$image['img_path']}}" width="110"
+                                             height="110" alt="商品图片">
+                                    </div>
+                                @endforeach
+                            @endif
                         </div>
-                        <div class="swiper-button-next"><i
-                                    class="iconfont icon-arrow-right font-size-lg text-white"></i></div>
-                        <div class="swiper-button-prev"><i class="iconfont icon-arrow-left font-size-lg text-white"></i>
-                        </div>
+                        <div class="swiper-button-next"><i class="iconfont icon-arrow-right font-size-lg text-white"></i></div>
+                        <div class="swiper-button-prev"><i class="iconfont icon-arrow-left font-size-lg text-white"></i></div>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="p-a-20x box-shadow bg-white">
-                    <h4 class="product-title helveBold">Aurora Ring</h4>
-                    <p class="m-b-15x text-primary">Boasting a spectacular display of color, the titanium Aurora ring
-                        stands out
-                        as a beautiful
-                        anomaly contemporary fashion. Be light-years ahead!</p>
+                    <h4 class="product-title helveBold">{{ $data['main_title'] }}</h4>
+                    <p class="m-b-15x text-primary"><?php  echo str_replace("\n", "<br/>",  $data['intro_short']) ?></p>
                     <div class="product-price">
-                        <span class="sanBold p-r-10x text-primary">$ 199.95</span>
-                        <span class="sanBold font-size-lxx text-common text-throughLine">$299.95</span>
+                        @if(isset($data['skuPrice']['skuPromotion']))
+                            <span class="sanBold p-r-10x text-primary">${{ number_format(($data['skuPrice']['skuPromotion']['promot_price'] / 100), 2) }}</span>
+                            <span class="sanBold font-size-lxx text-common text-throughLine">${{ number_format(($data['skuPrice']['skuPromotion']['price'] /100), 2) }}</span>
+                        @else
+                            <span class="sanBold p-r-10x text-primary">${{ number_format(($data['skuPrice']['sale_price'] / 100), 2) }}</span>
+                        @endif
                     </div>
                     <hr class="hr-common">
-                    <fieldset class="text-left m-b-20x">
-                        <div class="text-primary font-size-md">Size</div>
-                        <div class="m-l-15x">
-                            <div class="option-item">
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="size" value="xxl" id="xxl" hidden>
-                                    <label for="xxl" class="btn btn-itemProperty btn-sm" data-spa=""
-                                           data-ska="">XXL</label>
+                    @if(isset($data['spuAttrs']))
+                        @foreach($data['spuAttrs'] as $spuAttr)
+                            <fieldset class="text-left m-b-20x">
+                                <div class="text-primary font-size-md">{{$spuAttr['attr_type_value']}}:</div>
+                                <div class="m-l-15x">
+                                    <div class="option-item">
+                                        @foreach($spuAttr['skuAttrValues'] as $skuAttrValue )
+                                        <div class="p-y-5x p-r-10x">
+                                            <input type="radio" name="size" value="xxl" id="xxl" @if(!$skuAttrValue['stock']) disabled="disabled" @endif hidden>
+                                            <label for="xxl" class="btn btn-itemProperty btn-sm @if(!$skuAttrValue['stock']) disabled @endif" data-spa="" data-ska="">{{$skuAttrValue['attr_value']}}</label>
+                                        </div>
+                                        @endforeach
+                                    </div>
                                 </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="size" value="xl" id="xl" disabled="disabled" hidden>
-                                    <label for="xl" class="btn btn-itemProperty btn-sm disabled" data-spa=""
-                                           data-ska="">XL</label>
+                            </fieldset>
+                        @endforeach
+                    @endif
+                    @if(isset($data['vasBases']))
+                        @foreach($data['vasBases'] as $vas)
+                            <fieldset class="text-left m-b-20x">
+                                <div class="text-primary font-size-md">{{$vas['vas_describe']}} +${{number_format(($vas['vas_price'] / 100), 2)}}</div>
+                                <div class="m-l-15x">
+                                    <div class="p-y-5x flex flex-alignCenter">
+                                        <input type="text"
+                                               class="input-engraving form-control m-r-20x text-primary disabled">
+                                        <i class="iconfont icon-checkcircle text-primary font-size-lg"></i>
+                                    </div>
                                 </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="size" value="l" id="l" hidden>
-                                    <label for="l" class="btn btn-itemProperty btn-sm" data-spa="" data-ska="">L</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="size" value="s" id="s" hidden>
-                                    <label for="s" class="btn btn-itemProperty btn-sm" data-spa="" data-ska="">S</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="size" value="m" id="m" hidden>
-                                    <label for="m" class="btn btn-itemProperty btn-sm" data-spa="" data-ska="">M</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="size" value="xs" id="xs" hidden>
-                                    <label for="xs" class="btn btn-itemProperty btn-sm" data-spa=""
-                                           data-ska="">XS</label>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                    <fieldset class="text-left m-b-20x">
-                        <div class="text-primary font-size-md">Color</div>
-                        <div class="m-l-15x">
-                            <div class="option-item">
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="color" value="red" id="red" hidden>
-                                    <label for="red" class="btn btn-itemProperty btn-sm" data-spa=""
-                                           data-ska="">Red</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="color" value="blue" id="blue" hidden>
-                                    <label for="blue" class="btn btn-itemProperty btn-sm" data-spa=""
-                                           data-ska="">Blue</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="color" value="black" id="black" disabled="disabled"
-                                           hidden>
-                                    <label for="black" class="btn btn-itemProperty btn-sm disabled" data-spa=""
-                                           data-ska="">Black</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="color" value="green" id="green" hidden>
-                                    <label for="green" class="btn btn-itemProperty btn-sm" data-spa=""
-                                           data-ska="">Green</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="color" value="gold" id="gold" hidden>
-                                    <label for="gold" class="btn btn-itemProperty btn-sm" data-spa=""
-                                           data-ska="">Gold</label>
-                                </div>
-                                <div class="p-y-5x p-r-10x">
-                                    <input type="radio" name="color" value="darkBlue" id="darkBlue" hidden>
-                                    <label for="darkBlue" class="btn btn-itemProperty btn-sm" data-spa="" data-ska="">Dark
-                                        Blue</label>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                    <fieldset class="text-left m-b-20x">
-                        <div class="text-primary font-size-md">Ring Inside Engraving +$4.5</div>
-                        <div class="m-l-15x">
-                            <div class="p-y-5x flex flex-alignCenter">
-                                <input type="text" class="input-engraving form-control m-r-20x text-primary disabled">
-                                <i class="iconfont icon-checkcircle text-primary font-size-lg"></i>
-                            </div>
-                        </div>
-                    </fieldset>
+                            </fieldset>
+                        @endforeach
+                    @endif
                     <fieldset class="text-left m-b-20x">
                         <div class="flex flex-alignCenter">
                             <div class="text-primary font-size-md m-r-20x">Gift package+＄4.5(optional)</div>
@@ -180,12 +117,13 @@
     </div>
 </section>
 
+@if(isset($data['designer']))
 <div class="container m-t-30x">
     <span class="sanBold font-size-md p-x-20x">Designer:</span>
-  <span class="p-r-10x"><img class="img-circle" src="/images/icon/apple-touch-icon.png" width="40" height="40"
-                             alt=""></span>
-    <span class="sanBold text-main">Vivian</span>
+    <span class="p-r-10x"><img class="img-circle" src="/images/icon/apple-touch-icon.png" width="40" height="40" alt=""></span>
+    <span class="sanBold text-main">{{ $data['designer']['designer_name'] }}</span>
 </div>
+@endif
 
 <div class="container m-t-30x">
     <ul class="nav nav-tabs">
@@ -215,64 +153,27 @@
 <div class="container m-t-30x m-b-40x">
     <h4 class="helveBold text-main p-l-10x">You May Also Like</h4>
     <div class="row p-t-20x">
+        @foreach($recommended['list'] as $list)
         <div class="col-md-3 col-xs-6">
             <div class="productList-item">
                 <div class="image-container">
-                    <img class="img-fluid" src="/images/product/product.jpg" alt="商品的名称">
+                    <img class="img-fluid" src="{{config('common.APP_Api_Image')}}/n1/{{ $list['main_image_url']}}" alt="商品的名称">
                 </div>
                 <div class="price-caption helveBold">
-                    <div class="text-center font-size-md text-primary">New Rings</div>
+                    <div class="text-center font-size-md text-primary">{{ $list['main_title'] }}</div>
                     <div class="text-center">
-                        <span class="font-size-md text-primary p-r-5x">$199.95</span>
-                        <span class="font-size-base text-common text-throughLine">$299.95</span>
+                        <span class="font-size-md text-primary p-r-5x">${{ number_format(($list['skuPrice']['sale_price'] / 100), 2) }}</span>
+                        @if($list['skuPrice']['sale_price'] != $list['skuPrice']['price'])
+                            <span class="font-size-base text-common text-throughLine">${{ number_format(($list['skuPrice']['price'] / 100), 2) }}</span>
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-xs-6">
-            <div class="productList-item">
-                <div class="image-container">
-                    <img class="img-fluid" src="/images/product/product.jpg" alt="商品的名称">
-                </div>
-                <div class="price-caption helveBold">
-                    <div class="text-center font-size-md text-primary">New Rings</div>
-                    <div class="text-center">
-                        <span class="font-size-md text-primary p-r-5x">$199.95</span>
-                        <span class="font-size-base text-common text-throughLine">$299.95</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <div class="productList-item">
-                <div class="image-container">
-                    <img class="img-fluid" src="/images/product/product.jpg" alt="商品的名称">
-                </div>
-                <div class="price-caption helveBold">
-                    <div class="text-center font-size-md text-primary">New Rings</div>
-                    <div class="text-center">
-                        <span class="font-size-md text-primary p-r-5x">$199.95</span>
-                        <span class="font-size-base text-common text-throughLine">$299.95</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <div class="productList-item">
-                <div class="image-container">
-                    <img class="img-fluid" src="/images/product/product.jpg" alt="商品的名称">
-                </div>
-                <div class="price-caption helveBold">
-                    <div class="text-center font-size-md text-primary">New Rings</div>
-                    <div class="text-center">
-                        <span class="font-size-md text-primary p-r-5x">$199.95</span>
-                        <span class="font-size-base text-common text-throughLine">$299.95</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
-    <div class="text-center m-y-30x"><a class="btn btn-block btn-gray btn-lg btn-seeMore" href="#">See more of all</a>
+    <div class="text-center m-y-30x">
+        <a class="btn btn-block btn-gray btn-lg btn-seeMore" href="#">See more of all</a>
     </div>
     <div class="loading" style="display: none">
         <div class="loader">
