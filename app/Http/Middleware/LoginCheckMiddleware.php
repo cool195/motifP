b0b5c867248dbe534354adb4adef4d59348d7b98
zhangtao <250574087@qq.com>
2016-07-26 14:42:58 +0800

    头像，姓名

--- app/Http/Middleware/LoginCheckMiddleware.php
+++ app/Http/Middleware/LoginCheckMiddleware.php
@@ -8,6 +8,10 @@ class LoginCheckMiddleware
 {
     public function handle($request, Closure $next)
     {
+        if(Session::has('user')){
+            return $next($request);
+        }
+        //return redirect('/login?url='.urlencode($_SERVER['REQUEST_URI']));
         return $next($request);
     }
 }
