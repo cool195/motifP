2774985b8defdc4faf8878f26c505bf9d79844f5
motif <dinghuihua@evermarker.net>
2016-08-23 10:20:18 +0800

    update daily瀑布流

--- resources/views/daily/index.blade.php
+++ resources/views/daily/index.blade.php
@@ -21,14 +21,14 @@
 <div class="clearfix"></div>
 
 <!-- 列表内容 -->
-<div class="container m-b-40x" role="main" id="dailyList-container" data-pagenum="1" data-loading="false">
+<div class="container m-b-40x" role="main" id="dailyList-container" data-pagenum="0" data-loading="false">
     @if(!empty($list))
         <ul class="tiles-wrap animated row" id="daily-wookmark">
             @foreach($list as $daily)
-            <li>
+            <li class="isHidden">
                 <div class="daily-item">
                     <a href="@if(1 == $daily['skipType'])/product/@elseif(2==$daily['skipType'])/designer/@elseif(3==$daily['skipType'])/topic/@else/shopping/@endif{{ $daily['skipId'] }}">
-                        <img src="{{config('runtime.Image_URL')}}/images/product/bg-product@336.png" data-original="{{config('runtime.CDN_URL')}}/n0/{{$daily['imgPath']}}" class="img-fluid img-daily img-lazy">
+                        <img src="{{config('runtime.CDN_URL')}}/n2/{{$daily['imgPath']}}" class="img-fluid img-daily img-lazy">
                     </a>
                         @if(!empty($daily['title'] || !empty($daily['subTitle'])))
                         <div class="daily-info p-a-10x text-left">
@@ -38,17 +38,17 @@
                             </div>
                         </div>
                         @endif
-                            {{--<hr class="hr-base m-y-10x">
-                        <div class="flex flex-fullJustified flex-alignCenter">
-                            <div class="flex flex-alignCenter">
-                                <img src="{{config('runtime.Image_URL')}}/images/daily/daily.jpg" class="img-circle" width="30" height="30">
-                                <span class="p-l-15x">
-                                    <h6 class="text-main font-size-sm helveBold">Street Art</h6>
-                                    <a class="text-primary font-size-sm" href="#">facebook.com</a>
-                                </span>
-                            </div>
-                            <i class="iconfont icon-follow font-size-lx"></i>
-                        </div>--}}
+                            {{--<hr class="hr-base m-y-10x">--}}
+                        {{--<div class="flex flex-fullJustified flex-alignCenter">--}}
+                            {{--<div class="flex flex-alignCenter">--}}
+                                {{--<img src="{{config('runtime.Image_URL')}}/images/daily/daily.jpg" class="img-circle" width="30" height="30">--}}
+                                {{--<span class="p-l-15x">--}}
+                                    {{--<h6 class="text-main font-size-sm helveBold">Street Art</h6>--}}
+                                    {{--<a class="text-primary font-size-sm" href="#">facebook.com</a>--}}
+                                {{--</span>--}}
+                            {{--</div>--}}
+                            {{--<i class="iconfont icon-follow font-size-lx"></i>--}}
+                        {{--</div>--}}
                 </div>
             </li>
             @endforeach
@@ -59,25 +59,26 @@
         </div>
         <div class="loading daily-loading" style="display: none">
             <div class="loader"></div>
-            <div class="text-center p-t-10x">Loading...</div>
+            <div class="text-center p-t-5x p-l-10x">Loading...</div>
         </div>
 </div>
 
 <template id="tpl-daily">
     @{{ each list }}
-    <li>
+    <li class="isHidden">
         <div class="daily-item">
-            <a href="@if(1 == $daily['skipType'])/product/@elseif(2==$daily['skipType'])/designer/@elseif(3==$daily['skipType'])/topic/@else/shopping/@endif{{ $daily['skipId'] }}">
-                <img src="{{config('runtime.Image_URL')}}/images/product/bg-product@336.png" data-original="{{config('runtime.CDN_URL')}}/n0/{{$daily['imgPath']}}" class="img-fluid img-daily img-lazy">
+            <a href="@{{if $value.skipType == 1}}/product/@{{ else if $value.skipType == 2 }}/designer/@{{ else if $value.skipType == 3 }}/topic/@{{ else if $value.skipType == 4}}/shopping/@{{ /if }}@{{ $value.skipId }}">
+                <img src="{{config('runtime.CDN_URL')}}/n2/@{{ $value.imgPath }}" class="img-fluid img-daily">
             </a>
-            @if(!empty($daily['title'] || !empty($daily['subTitle'])))
+
+            @{{ if undefined !== ( $value.title || $value.subTitle ) }}
                 <div class="daily-info p-a-10x text-left">
                     <div>
-                        <h6 class="text-main helveBold font-size-md m-b-5x">{{$daily['title']}}</h6>
-                        <p class="text-primary m-b-0">{{ $daily['subTitle'] }}</p>
+                        <h6 class="text-main helveBold font-size-md m-b-5x">@{{ $value.title }}</h6>
+                        <p class="text-primary m-b-0">@{{ $value.subTitle }}</p>
                     </div>
                 </div>
-            @endif
+            @{{ /if }}
         </div>
     </li>
     @{{ /each }}
