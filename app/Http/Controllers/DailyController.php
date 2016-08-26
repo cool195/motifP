204f2dae8394910b0ccf9f964393efc8f8764f13
zhangtao <250574087@qq.com>
2016-08-26 17:14:30 +0800

    代码修改

--- app/Http/Controllers/DailyController.php
+++ app/Http/Controllers/DailyController.php
@@ -74,7 +74,7 @@ class DailyController extends BaseController
             $view = 'daily.topic';
         }
 
-        return View($view, ['topic' => $result['data'], 'topicID' => $id, 'shareFlag' => false]);
+        return $result; //View($view, ['topic' => $result['data'], 'topicID' => $id, 'shareFlag' => false]);
     }
 
 }
