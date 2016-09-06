d96eec8805749c2e9fad32e5b8fb7bde47bed212
lhyin <993406018@qq.com>
2016-09-06 10:51:37 +0800

    update

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -184,7 +184,7 @@
                             <a href="javascript:void(0);" id="productAddBag"
                                class="btn btn-primary btn-lg btn-350 btn-addToBag @if($data['isPutOn']==0){{'disabled'}}@endif">Add to Bag</a>
                         @else
-                            <a href="/login" class="btn btn-block btn-primary btn-lg btn-350 btn-addToBag">Add to Bag</a>
+                            <a href="/login" class="btn btn-primary btn-lg btn-350 btn-addToBag">Add to Bag</a>
                         @endif
                     </div>
                     </div>
