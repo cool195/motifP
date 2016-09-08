6999bdde67cc08acaf02dcec3d50e8cfa49efee7
zhangtao <250574087@qq.com>
2016-09-08 16:10:54 +0800

    bug fixed

--- resources/views/order/orderlist.blade.php
+++ resources/views/order/orderlist.blade.php
@@ -20,11 +20,11 @@
                             @foreach($data['list'] as $order)
                                 @foreach($order['subOrderList'] as $subOrder)
                                     <div class="box-shadow bg-white m-b-20x order-item">
-                                        <span class="horn  @if(11 == $subOrder['status_code']) horn-red
+                                        <span class="horn @if(11 == $subOrder['status_code']) horn-red
                                         @elseif(in_array($subOrder['status_code'], array(14, 24))) horn-orange
                                         @elseif(18 == $subOrder['status_code']) horn-green
                                         @elseif(in_array($subOrder['status_code'], array(19, 20))) horn-blue
-                                        @elseif(25 == $subOrder['status_code]) horn-lightblue
+                                        @elseif(25 == $subOrder['status_code']) horn-lightblue
                                         @elseif(in_array($subOrder['status_code'], array(21, 22, 23))) horn-gray
                                         @endif"></span>
                                         <div class="p-x-20x p-y-15x flex flex-alignCenter flex-fullJustified">
