bf1888d1d210c6e479f13e10f59301434b7f4992
zhangtao <250574087@qq.com>
2016-09-09 18:56:04 +0800

    曝光埋点成功发送

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -39,6 +39,81 @@ function HideSeeMore(seemoreName) {
         });
     }
 
+    // public 公共方法start
+
+    function switchImpr(Impr) {
+        // 当前视窗浏览位置
+        var CurrentPosition = $(window).scrollTop() + $(window).height();
+        // 元素本身聚顶部高度
+        var ItemPosition = $(Impr).offset().top;
+
+        // 如果是曝光项，加上本身的高度
+        // 完全出现在视窗内时，再曝光
+        if ($(Impr).is('a')) {
+            ItemPosition += $(Impr).height();
+        }
+
+        // 判断是否在视窗内
+        if (ItemPosition <= CurrentPosition) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * pushAjax - 依次发送 ajax 请求，遍历所有项
+     *
+     * @param  { Object } $Item 需要曝光的项
+     */
+    function pushAjax($Item) {
+        $.each($Item, function (index, element) {
+            if (switchImpr(element)) {
+                var impr = $(element).data('impr');
+                $(element).removeAttr('data-impr');
+
+                $.ajax({
+                        url: impr
+                    })
+                    .always(function () {
+                        //$(element).removeAttr('data-impr');
+                    });
+            }
+        });
+    }
+
+    function imprList() {
+        var $ImprList = $('[data-impr]');
+        if ($ImprList.length !== 0) {
+            pushAjax($ImprList);
+        }
+    }
+
+    $(document).on('scroll', function (event) {
+        imprList();
+    })
+
+    $(document).ready(function () {
+        $('[data-clk]').unbind('click');
+        $('[data-clk]').click(function () {
+            var $this = $(this);
+
+            //onProductClick();
+
+            if(undefined != $this.data('link')) {
+                $.ajax({
+                    url: $this.data('link'),
+                    type: "GET"
+                });
+                setTimeout(function () {
+                    window.location.href = $this.data('link');
+                }, 100);
+            }
+        })
+    })
+
+    // public 公共方法end
+
 
     // ShoppingDetail.html  start
 
@@ -109,7 +184,7 @@ function HideSeeMore(seemoreName) {
             $('#skuQty').parent().siblings('.warning-info').addClass('off');
             $('#delQtySku').addClass('disabled');
             //todo 加判断
-            $('#addQtySku').removeClass('disabled');
+            //$('#addQtySku').removeClass('disabled');
             if ($(this).hasClass('active')) {
                 $(this).removeClass('active');
                 delete product_arrayTemp_click['key' + $(this).attr('data-attr-type')];
