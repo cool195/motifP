4a652251fbb11381c47aa8bc8dea945a4fc077d0
zhangtao <250574087@qq.com>
2016-09-13 19:31:05 +0800

    bug fixed

--- resources/views/user/foot.blade.php
+++ resources/views/user/foot.blade.php
@@ -4,5 +4,3 @@
         <div class="text-center p-t-10x m-b-10x text-primary font-size-xs">Copyright © 2016 MOTIF Inc. All rights reserved.</div>
     </div>
 </footer>
-<script src="/scripts/vendor.js"></script>
-<script src="/scripts/common.js"></script>
