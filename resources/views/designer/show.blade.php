0dfe4fa58822e045493897ff608f98c649c5a4c9
lhyin <993406018@qq.com>
2016-09-14 15:45:51 +0800

    update designer detail 增加社交图标

--- resources/views/designer/show.blade.php
+++ resources/views/designer/show.blade.php
@@ -57,6 +57,12 @@
                             @if(!empty($designer['instagram_link']))
                                 <a href="{{$designer['instagram_link']}}" class="m-r-20x"><img src="{{config('runtime.Image_URL')}}/images/icon/icon-ins.png"></a>
                             @endif
+                            @if(!empty($designer['youtube_link']))
+                                <a href="{{$designer['youtube_link']}}" class="m-r-20x"><img src="{{config('runtime.Image_URL')}}/images/icon/icon-youtube@2x.png"></a>
+                            @endif
+                            @if(!empty($designer['blog_link']))
+                                <a href="{{$designer['blog_link']}}" class="m-r-20x"><img src="{{config('runtime.Image_URL')}}/images/icon/icon-blog@2x.png"></a>
+                            @endif
                         @if(!empty($designer['instagram_link']) || !empty($designer['snapchat_link']) || !empty($designer['youtube_link']) || !empty($designer['facebook_link']))
                         </div>
                         @endif
