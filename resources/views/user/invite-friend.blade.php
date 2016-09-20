981307c5643b8da88018766ac00bf00217198c8e
lhyin <993406018@qq.com>
2016-09-20 14:45:34 +0800

    update 新增 inviteFriend 页面 及样式

--- resources/views/user/invite-friend.blade.php
+++ resources/views/user/invite-friend.blade.php
@@ -4,22 +4,29 @@
 <section class="m-y-40x">
     <div class="container box-shadow bg-white">
         <div class="invite-content">
-            <div class="invite-title">Share Promotion Code with your friends</div>
+            <div class="invite-title helveBold">Share Promotion Code with your friends</div>
             <p class="p-y-20x text-primary font-size-md">Both you and your friends will get $20 off after their first purchase!
                 <a href="#" class="text-link text-underLine">Detail</a></p>
-            <div class="flex">
-                <span class="sanBold font-size-md">Invite Code:</span>
-                <span class="input-group">
-                    <input type="text" class="form-control input-engraving" placeholder="Search for...">
-                  <span class="input-group-btn">
-                    <button class="btn btn-secondary" type="button">Go!</button>
-                  </span>
-                </span>
-                <a href="/daily" class="btn btn-primary btn-lg btn-200">Invite Friends</a>
+            <div class="flex flex-alignCenter">
+                <span class="sanBold font-size-md p-r-15x">Invite Code:</span>
+                <div class="input-group invite-input p-r-30x">
+                    <input type="text" class="form-control" id="inviteCode" placeholder="Code" aria-describedby="btn-copy">
+                    <span class="input-group-addon text-primary font-size-md" id="btn-copy" onclick="copyinput();">Copy</span>
+                </div>
+                <span class="p-l-30x"><a href="/daily" class="btn btn-primary btn-lg btn-200">Invite Friends</a></span>
             </div>
         </div>
     </div>
 </section>
 
 @include('footer')
+<script type="text/javascript">
+    function copyinput()
+    {
+        var input=document.getElementById("inviteCode");//input的ID值
+        input.select(); //选择对象
+        document.execCommand("Copy"); //执行浏览器复制命令
+    }
+</script>
+
 
