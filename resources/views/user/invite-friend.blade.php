a8d5ed39b66fbea3b058306698d8ad3fa543d384
SuperJu <juchao1989@foxmail.com>
2016-09-20 18:55:10 +0800

    update

--- resources/views/user/invite-friend.blade.php
+++ resources/views/user/invite-friend.blade.php
@@ -14,13 +14,17 @@
                     <span class="input-group-addon text-primary font-size-md" id="btn-copy" onclick="copyinput();">Copy</span>
                 </div>
                 <span class="p-l-30x">
-                    <a target="_blank" href="http://www.facebook.com/share.php?u=http://pc.motif.me/d/invite/{{$code}}">fb</a>
-                    <a target="_blank" href="http://twitter.com/home/?status=http://pc.motif.me/d/invite/{{$code}}">tw</a>
+                    <a href="javascript:window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent('http://pc.motif.me/d/invite/{{$code}}'),'_blank','toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=600, height=450,top=100,left=350');void(0)">facebook</a>
+                    <a href="javascript:window.open('http://twitter.com/home?status='+encodeURIComponent('http://pc.motif.me/d/invite/{{$code}}'),'_blank','toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=600, height=450,top=100,left=350');void(0)" >twitter</a>
+                    <a href="//www.pinterest.com/pin/create/button/" data-pin-do="buttonBookmark"  data-pin-shape="round" data-pin-height="28">pinterest</a>
                 </span>
             </div>
         </div>
     </div>
 </section>
+
+
+<script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script></li>
 
 @include('footer')
 <script type="text/javascript">
