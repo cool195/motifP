509152f095f78283780465c9354e8a7fecad042a
lhyin <993406018@qq.com>
2016-09-20 17:04:12 +0800

    update invite

--- resources/views/user/invite.blade.php
+++ resources/views/user/invite.blade.php
@@ -31,15 +31,15 @@
     <div class="container p-y-30x">
         <div class="row">
             <div class="col-md-4 p-x-20x">
-                <div class="text-center"><i class="iconfont icon-user text-main font-size-llxx"></i></div>
+                <div class="text-center"><i class="iconfont icon-user text-main inviteBanner-subTitle"></i></div>
                 <div class="text-center font-size-lx text-main p-t-20x">Sign up & Register.</div>
             </div>
             <div class="col-md-4 p-x-20x">
-                <div class="text-center"><i class="iconfont icon-ticket text-main font-size-llxx"></i></div>
+                <div class="text-center"><i class="iconfont icon-ticket text-main inviteBanner-subTitle"></i></div>
                 <div class="text-center font-size-lx text-main p-t-20x">Enter promo code when you check to receive $20 odd your purchase.</div>
             </div>
             <div class="col-md-4 p-x-20x">
-                <div class="text-center"><i class="iconfont icon-user text-main font-size-llxx"></i></div>
+                <div class="text-center"><img src="{{config('runtime.Image_URL')}}/images/icon/icon-InviteFriends.png" alt=""></div>
                 <div class="text-center font-size-lx text-main p-t-20x">Share “Get $20 Off” from your Motif account profile to get even more credit.</div>
             </div>
         </div>
