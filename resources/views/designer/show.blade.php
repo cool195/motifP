229f685fc317b4ed704465e0a03ab1341c6a4939
motif <dinghuihua@evermarker.net>
2016-10-10 17:54:47 +0800

    update更新字体文件。修改未登录时，收藏样式的问题

--- resources/views/designer/show.blade.php
+++ resources/views/designer/show.blade.php
@@ -188,7 +188,7 @@
                                                     <i class="iconfont btn-wish font-size-lxx @if(in_array($spu, $wishlist->wishlist())) active @endif" data-spu="{{$spu}}"></i>
                                                 </span>
                                             @else
-                                                <a class="product-heart btn-heart" href="javascript:void(0)"><i class="iconfont btn-wish font-size-lxx" data-actionspu="{{$spu}}"></i></a>
+                                                <span class="product-heart btn-heart"><i class="iconfont btn-wish font-size-lxx" data-actionspu="{{$spu}}"></i></span>
                                             @endif
                                         </div>
                                         <div class="price-caption helveBold">
@@ -242,7 +242,7 @@
                         @if(Session::has('user'))
                             <span class="product-heart btn-heart"><i class="iconfont btn-wish font-size-lxx @if(in_array($product['spu'], $wishlist->wishlist())) active @endif" data-spu="{{$product['spu']}}"></i></span>
                         @else
-                            <a class="product-heart btn-heart" href="javascript:void(0)"><i class="iconfont btn-wish font-size-lxx" data-actionspu="{{$product['spu']}}"></i></a>
+                            <span class="product-heart btn-heart"><i class="iconfont btn-wish font-size-lxx" data-actionspu="{{$product['spu']}}"></i></span>
                         @endif
 
                     </div>
