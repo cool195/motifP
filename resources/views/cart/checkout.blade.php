565ea9cb8cf1eaa0c1b3c83599a479b72b939028
SuperJu <juchao1989@foxmail.com>
2016-10-13 14:35:56 +0800

    update

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -480,10 +480,12 @@
                 @{{ /if }}
             </div>
             {{--提示--}}
-            <span class="warning-info flex flex-alignCenter text-warning p-t-5x">
-                <i class="iconfont icon-caveat icon-size-md p-r-5x"></i>
-                <span class="font-size-base">This coupon can only be used in our free Motif app.</span>
-            </span>
+            @{{ if $value.bind_id == 678 }}
+                <span class="warning-info flex flex-alignCenter text-warning p-t-5x">
+                    <i class="iconfont icon-caveat icon-size-md p-r-5x"></i>
+                    <span class="font-size-base">This coupon can only be used in our free Motif app.</span>
+                </span>
+            @{{ /if }}
         </div>
     </div>
     @{{ /each }}
