c36a33441109c944edb9bcfdd175462a4b8389ae
lhyin <993406018@qq.com>
2016-11-07 11:43:55 +0800

    update payment

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -2,7 +2,7 @@
 
         <!-- 内容 -->
 <section class="m-y-40x">
-    <div class="container" id="addressView" data-status="true">
+    <div class="container" data-status="true">
         <div class="myHome-content">
             @include('user.left', ['title' => 'Payment Method'])
             @inject('Address', 'App\Http\Controllers\AddressController')
