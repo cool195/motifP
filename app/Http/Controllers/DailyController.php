d6b72f6e050f2e1a6282c7297a76bc00d235229e
SuperJu <juchao1989@foxmail.com>
2016-11-23 15:54:31 +0800

    update

--- app/Http/Controllers/DailyController.php
+++ app/Http/Controllers/DailyController.php
@@ -85,7 +85,7 @@ class DailyController extends BaseController
 
     public function staticShow($id)
     {
-        $result = $this->service($id);
+        $result = $this->service(null,$id);
 
         return View('daily.topic', ['topic' => $result['data'], 'topicID' => $id, 'shareFlag' => false]);
     }
