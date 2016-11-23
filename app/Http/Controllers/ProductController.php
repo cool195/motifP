009b532d44786a799351bc1d18161b4e2eeb7a6d
SuperJu <juchao1989@foxmail.com>
2016-11-23 14:01:12 +0800

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -97,7 +97,7 @@ class ProductController extends BaseController
                 foreach ($skuAttrValues['skus'] as $k3 => $sku) {
                     foreach ($result['skuExps'] as $skuExp) {
                         if ($skuExp['sku'] == $sku) {
-                            if ($skuExp['stock_qtty'] > 1) {
+                            if ($skuExp['stock_qtty'] > 0) {
                                 $tempSkus[] = $sku;
                             }
                         }
