143d53a5ccaf12342c6e703bf895eaea13eb80dd
lhyin <993406018@qq.com>
2016-11-24 12:02:12 +0800

    update

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -130,7 +130,7 @@ function HideSeeMore(seemoreName) {
     var redTimer;
     try{
         $(function(){
-            if($('#logged-user').length <= 0){
+            if($('#logged-user').length <= 0 && $('.login-container').length <=0 ){
                 if(getCookie('motifAted')) {
                     setRedTimer();
                 } else {
