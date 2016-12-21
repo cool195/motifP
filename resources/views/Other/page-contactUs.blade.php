220a30c057893d9f8ecbc6b347674454b98a10cb
zhangtao <250574087@qq.com>
2016-12-21 17:04:07 +0800

    静态页面左边公共部分

--- resources/views/Other/page-contactUs.blade.php
+++ resources/views/Other/page-contactUs.blade.php
@@ -3,31 +3,7 @@
 <section class="body-container m-y-20x">
     <div class="container">
         <div class="myHome-content">
-            <!--静态页 共用左侧导航-->
-            <div class="leftMeun m-t-40x p-t-30x">
-                <nav class="nav-menu p-b-15x">
-                    <ul class="nav font-size-md">
-                        <li class="nav-item">
-                            <a href="#">RETURN POLICY</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#">SHIPPING</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#">FAQ</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a href="#">CONTACT US</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#">TERMS & CONDITIONS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#">PRIVACY NOTICE</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            @include('Other.page-left')
             <div class="right">
                 <div class="rightContent">
                     <div class="text-center static-tit">LET'S BE<br>
