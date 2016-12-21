0037fe2e1be45ba35a9a4e27d79b3d65acb51d57
lhyin <993406018@qq.com>
2016-12-21 16:11:09 +0800

    update CONTACT SERVICE 页面样式

--- resources/views/Other/AskShopping.blade.php
+++ resources/views/Other/AskShopping.blade.php
@@ -1,9 +1,10 @@
 @include('header')
 <!--内容-->
-<div class="container">
+<div class="container body-container">
     <div class="content-wrap">
-        <h2 class="helveBold text-main font-size-lxx m-b-20x m-x-20x">Contact Service</h2>
-        <div class="bg-white content box-shadow">
+        <h2 class="bigNoodle m-b-20x text-center leftMeun-title">Contact Service</h2>
+        <hr class="hr-black m-t-0">
+        <div class="bg-white content">
             <form method="post" id="form-askQuestion" action="/askshopping">
                 <input type="hidden" class="form-control contrlo-lg text-primary" name="id" value="{{$id}}">
                 <input type="hidden" class="form-control contrlo-lg text-primary" name="skiptype" value="{{$skiptype}}">
@@ -11,9 +12,9 @@
                 <textarea class="form-control contrlo-lg text-primary" name="content" id="content"></textarea>
                 <br/>
                 <div class="row m-b-20x">
-                    <div class="text-right p-x-30x p-y-10x">
-                        <a  class="btn btn-primary btn-lg btn-200" href="{{Session::has('referer') ? Session::get('referer') : '/order/orderlist'}}">Cancel</a>
-                        <div class="btn btn-primary btn-lg btn-200" data-role="submit" data-spu="123" id="askSend">Send</div>
+                    <div class="text-right p-x-10x p-y-10x">
+                        <a  class="btn btn-primary btn-200 font-size-lxx text-white m-r-20x bigNoodle" href="{{Session::has('referer') ? Session::get('referer') : '/order/orderlist'}}">Cancel</a>
+                        <div class="btn btn-200 bigNoodle font-size-lxx btn-green" data-role="submit" data-spu="123" id="askSend">Send</div>
                     </div>
                 </div>
             </form>
