16170272c8dcac647b0114234bb839e8541494b8
zhangtao <250574087@qq.com>
2016-12-22 17:09:58 +0800

    home页面图片数据

--- resources/views/daily/home.blade.php
+++ resources/views/daily/home.blade.php
@@ -7,7 +7,7 @@
                     <div>
                         <a href="@if(1 == $value['banner_skip_type'])/detail/@elseif(2==$value['banner_skip_type'])/designer/@elseif(3==$value['banner_skip_type'])/topic/@elseif(4 == $value['banner_skip_type'])/shopping/@endif{{ $value['banner_skip'] }}">
                             <img class="img-fluid img-lazy figure"
-                                 data-original="{{config('runtime.CDN_URL')}}/n1/{{$value['imgPath']}}"
+                                 data-original="{{config('runtime.CDN_URL').'/n0/'.$value['img_path']}}"
                                  src="{{env('CDN_Static')}}/images/product/bg-product@750.png" alt="">
                         </a>
                     </div>
