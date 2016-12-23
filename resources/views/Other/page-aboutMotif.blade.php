1ce0fad7b86de6546abb3701bab3d2cfc775a05b
lhyin <993406018@qq.com>
2016-12-23 16:29:11 +0800

    update

--- resources/views/Other/page-aboutMotif.blade.php
+++ resources/views/Other/page-aboutMotif.blade.php
@@ -1,47 +1,6 @@
 @include('header', ['title' => 'About Motif'])
 
 <section class="body-container m-y-10x">
-    {{--<div class="container">--}}
-        {{--<div class="myHome-content">--}}
-            {{--<!--静态页 共用左侧导航-->--}}
-            {{--@include('Other.page-left', ['title' => 'aboutmotif'])--}}
-            {{--<div class="right">--}}
-                {{--<div class="rightContent">--}}
-                    {{--<div class="bigNoodle text-center leftMeun-title uppercase">about motif</div>--}}
-                    {{--<hr class="hr-black m-t-0">--}}
-                    {{--<div class="row text-center">--}}
-                        {{--<div class="p-a-30x">--}}
-                            {{--<p class="m-a-0">Your style is unique and cutting edge - your fashion should be too. </p>--}}
-                            {{--<p class="m-a-0">Exclusive, limited edition accessories designed by the world’s top--}}
-                                {{--fashion--}}
-                                {{--bloggers, Instagrammers and digital influencers are all here.</p>--}}
-                            {{--<p class="m-a-0">Motif products and services include: </p>--}}
-                            {{--<p class="m-a-0">- No. 1 ecommerce fashion boutique for digital influencers and--}}
-                                {{--limited--}}
-                                {{--editions</p>--}}
-                            {{--<p class="m-a-0">- Collections by the world’s best fashion and beauty Instagrammers,--}}
-                                {{--bloggers--}}
-                                {{--and YouTubers </p>--}}
-                            {{--<p class="m-a-0">- Limited edition collections: sold out designs will never be--}}
-                                {{--reproduced</p>--}}
-                            {{--<p class="m-a-0">- Also featuring hand-picked emerging fashion designers from around--}}
-                                {{--the--}}
-                                {{--world</p>--}}
-                            {{--<p class="m-a-0">- Rare, never before seen fashion: make your style as unique and--}}
-                                {{--special as--}}
-                                {{--you are</p>--}}
-                            {{--<p class="m-a-0">- New designs and style inspiration on our daily newsfeed</p>--}}
-                            {{--<p class="m-a-0">- NEW style inspo from the Motif social community, and be featured on--}}
-                                {{--our app--}}
-                                {{--via #motifme</p>--}}
-                            {{--<p class="m-a-0">- Free shipping and returns</p>--}}
-                            {{--<p class="m-a-0">Download the app. Discover your personal Motif. Flaunt.</p>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
     <div class="content-wrap aboutUs-content">
         <h1 class="bigNoodle text-main text-center m-b-40x">NBD. JUST CHANGING THE FACE OF FASHION</h1>
 
