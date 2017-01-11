4f320be6f28130d8c11ec7f745af30c379a87b48
motif <dinghuihua@evermarker.net>
2017-01-11 16:40:47 +0800

    update  404页面

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,13 +4,14 @@
 <section class="m-y-40x">
     <div class="container text-center p-y-40x">
         <div class="order_comfirmed_content">
-            <img src="{{config('runtime.Image_URL')}}/images/error/404@2x.png" srcset="{{config('runtime.Image_URL')}}/images/error/404@3x.png 2x">
-            <h3 class="helveBold m-b-20x m-t-30x text-primary">Seems like you're lost…</h3>
-            <p class="p-t-20x">
+            <div class="bigNoodle error-text">404</div>
+{{--            <img src="{{config('runtime.Image_URL')}}/images/error/404@2x.png" srcset="{{config('runtime.Image_URL')}}/images/error/404@3x.png 2x">--}}
+            <h3 class="text-primary">Seems like you're lost…</h3>
+            <p class="p-t-30x">
                 Your requested URL was not found<br>
                 You may want to
             </p>
-            <a href="/daily" class="btn btn-primary btn-lg btn-350">Go Home</a>
+            <a href="/daily" class="btn btn-primary font-size-llx bigNoodle btn-300">Go Home</a>
         </div>
     </div>
 </section>
