e075f62bfe99a07e002f55ea4c8b6a3d36dc7063
motif <dinghuihua@evermarker.net>
2017-01-13 14:24:21 +0800

    修改404页面样式

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,12 +6,12 @@
         <div class="order_comfirmed_content">
             <div class="bigNoodle error-text">404</div>
 {{--            <img src="{{config('runtime.Image_URL')}}/images/error/404@2x.png" srcset="{{config('runtime.Image_URL')}}/images/error/404@3x.png 2x">--}}
-            <h3 class="text-primary">Seems like you're lost…</h3>
+            <h3 class="text-primary bigNoodle">Seems like you're lost…</h3>
             <p class="p-t-30x">
                 Your requested URL was not found<br>
                 You may want to
             </p>
-            <a href="/daily" class="btn btn-primary font-size-llx bigNoodle btn-300">Go Home</a>
+            <a href="/daily" class="btn btn-green font-size-llx bigNoodle btn-300">Go Home</a>
         </div>
     </div>
 </section>
