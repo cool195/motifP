118666089c60bc9d61eea24057deb935bd7d31fe
zhangtao <250574087@qq.com>
2017-01-20 14:56:31 +0800

    bug fixed

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -234,6 +234,7 @@
 </div>--}}
 <script src="{{config('runtime.Image_URL')}}/scripts/vendor.js{{config('runtime.V')}}"></script>
 <script src="{{config('runtime.Image_URL')}}/scripts/card.js{{config('runtime.V')}}"></script>
+<script src="https://apis.google.com/js/api:client.js"></script>
 <script src="{{config('runtime.Image_URL')}}/scripts/common.js{{config('runtime.V')}}"></script>
 @if (env('APP_ENV') == 'production')
 <script src="{{config('runtime.CLK_URL')}}/wl.js"></script>
