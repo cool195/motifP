25f72af64c5142410a709933adc182fae41a8b81
zhangtao <250574087@qq.com>
2017-02-24 15:03:45 +0800

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -117,6 +117,8 @@ class AuthController extends BaseController
         );
         $result = $this->request('openapi', '', "user", $params);
         $result['status'] = $result['data']['email'] ? true : false;
+        error_log(print_r("------------------\n", "\n"), 3, '/tmp/myerror.log');
+        error_log(print_r($result, "\n"), 3, '/tmp/myerror.log');
         return $result;
     }
 
