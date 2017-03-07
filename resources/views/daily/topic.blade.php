4a1322978d61375abbdddd4ee160d3499181798f
zhangtao <250574087@qq.com>
2017-03-07 15:23:12 +0800

    bug fixed

--- resources/views/daily/topic.blade.php
+++ resources/views/daily/topic.blade.php
@@ -137,6 +137,11 @@
                                                      src="{{env('CDN_Static')}}/images/product/bg-product@336.png"
                                                      alt="{{$topic['spuInfos'][$spu]['spuBase']['main_title']}}">
                                             </a>
+                                            @if(0 == $topic['spuInfos'][$spu]['spuBase']['isPutOn'])
+                                            <div class="bg-soldout">
+                                                <span class="text bigNoodle font-size-sm">SOLD OUT</span>
+                                            </div>
+                                            @endif
                                             @if(Session::has('user'))
                                                 <span class="product-heart btn-heart">
                                                     <i class="iconfont btn-wish font-size-lxx @if(in_array($spu, $wishlist->wishlist())) active @endif" data-spu="{{$spu}}"></i>
