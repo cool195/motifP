ca0fb518d0a5b09d319996332c55e9c085988f56
zhangtao <250574087@qq.com>
2017-03-08 10:59:05 +0800

    bug fixed

--- resources/views/daily/topic.blade.php
+++ resources/views/daily/topic.blade.php
@@ -136,7 +136,7 @@
                                                      data-original="{{config('runtime.CDN_URL')}}/n1/{{$topic['spuInfos'][$spu]['spuBase']['main_image_url']}}"
                                                      src="{{env('CDN_Static')}}/images/product/bg-product@336.png"
                                                      alt="{{$topic['spuInfos'][$spu]['spuBase']['main_title']}}">
-                                                    @if(0 == $topic['spuInfos'][$spu]['spuBase']['isPutOn'])
+                                                    @if($topic['spuInfos'][$spu]['spuBase']['isPutOn'] != 1)
                                                         <div class="bg-soldout">
                                                             <span class="text bigNoodle font-size-sm">SOLD OUT</span>
                                                         </div>
